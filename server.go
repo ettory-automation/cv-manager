e145f8ecbed33964f779310805f639b99d650539
Ettory Martins de Souza <ettory.devops@gmail.com>
2025-11-16 04:48:25 -0300

    feat: add config package with environment definitions

--- server.go
+++ server.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/ettory-automation/cv-manager/config"
 	"github.com/ettory-automation/cv-manager/generated"
 	"github.com/ettory-automation/cv-manager/resolvers"
 	"github.com/go-chi/chi"
@@ -20,9 +20,10 @@ import (
 const defaultPort = "8080"
 
 func main() {
-	port := os.Getenv("PORT")
+	env := config.GetEnv()
+	port := env.Port
 	if port == "" {
-		port = defaultPort
+		port = config.DEFAULT_PORT
 	}
 
 	router := chi.NewRouter()
